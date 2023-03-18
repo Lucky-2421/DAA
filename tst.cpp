c10cf4a5e1e7ee7bbf1594bd32265abb8dcefcdd
Lucky2421 <128221432+Lucky2421@users.noreply.github.com>
2023-03-18 15:24:28 +0530

    C++

---
+++ tst.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class user
+	{
+private:{
+		string name;
+		int age;
+		string address;
+		
+		}
+public:
+		 user(string name,int age,string address)
+		{
+		name=name;
+		age=age;
+		address=address;
+		}
+		void printdetails()
+		{
+		cout<<"Name:"name<<endl;
+		cout<<"Age:"<<age<<endl;
+		cout<<"Address:"<<address<<endl;
+		}
+int main()
+string name,address;
+int age:
+cout<<"Enter your name:";
+cin>>name;
+cout<<"Enter your age:";
+cout<<"ENter your address:";
+cin>>address;
+
+user(name,age,address);
+
+cout<<"User details:"<<endl;
+user.printdetails();
+
+return 0;
+}
+
+
